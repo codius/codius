22541416251d43d172fac345609e43b291428843
Brandon Wilson <brandon@coil.com>
2024-04-22 12:32:20 -0500

    chore: add typescript and meta-updater

---
+++ .meta-updater/main.mjs
@@ -0,0 +1,39 @@
+import { existsSync, readdirSync } from "node:fs"
+import { join } from "node:path"
+
+export default (workspaceDir) => {
+  return {
+    "tsconfig.json": (tsConfig, { manifest, dir }) => {
+      if (!tsConfig) return tsConfig
+
+      if (manifest.name === "@codius/root") {
+        const packages = readdirSync(join(dir, "packages"))
+
+        const packagesWithTsconfig = packages.filter((packageName) => {
+          const tsConfigPath = join(
+            dir,
+            "packages",
+            packageName,
+            "tsconfig.json",
+          )
+          return existsSync(tsConfigPath)
+        })
+
+        return {
+          ...tsConfig,
+          references: packagesWithTsconfig.map((packageName) => ({
+            path: `./packages/${packageName}/tsconfig.json`,
+          })),
+        }
+      }
+
+      return {
+        ...tsConfig,
+        compilerOptions: {
+          ...tsConfig.compilerOptions,
+          outDir: "dist",
+        },
+      }
+    },
+  }
+}
